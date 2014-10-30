bed81893dcab3534a2e242c8e5e9ede890e6968a
Taylor Fausak <taylor@fausak.me>
2014-10-29 22:33:01 -0500

    Add some fields to the entry model

--- library/Ledger/Models/Entry.hs
+++ library/Ledger/Models/Entry.hs
@@ -1,12 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Ledger.Models.Entry
   ( Entry (..)
   ) where
 
-import           Data.Aeson (ToJSON, object, toJSON)
+import           Data.Aeson (ToJSON, object, toJSON, (.=))
+import           Data.Time  (UTCTime)
 
 data Entry = Entry
+  { amount  :: Rational
+  , created :: UTCTime
+  , number  :: Integer
+  }
 
 instance ToJSON Entry where
-  toJSON _entry = object
-    [
+  toJSON entry = object
+    [ "amount" .= (fromRational (amount entry) :: Double)
+    , "created" .= created entry
+    , "number" .= number entry
     ]
