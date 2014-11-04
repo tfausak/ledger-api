4499d20ff5a5f648d2bec7a9ad887d3a7de92335
Taylor Fausak <taylor@fausak.me>
2014-11-03 22:39:50 -0600

    Clean up routing

--- library/Ledger/Router.hs
+++ library/Ledger/Router.hs
@@ -19,33 +19,17 @@ route request =
   let path = pathInfo request
       method = requestMethod request
   in  case path of
-        [] -> static method "ledger.html" "text/html"
-        ["jsx.js"] -> static method "vendor/JSXTransformer-0.12.0.js" "application/javascript"
-        ["ledger.css"] -> static method "ledger.css" "text/css"
-        ["ledger.jsx"] -> static method "ledger.jsx" "text/jsx"
-        ["ledger.manifest"] -> static method "ledger.manifest" "text/cache-manifest"
-        ["react.js"] -> static method "vendor/react-0.12.0.js" "application/javascript"
-        ["superagent.js"] -> static method "vendor/superagent-0.20.0.js" "application/javascript"
+        -- Front end
+        [] -> static method
+          "ledger.html" "text/html"
+        ["ledger.manifest"] -> static method
+          "ledger.manifest" "text/cache-manifest"
+        ["ledger.css"] -> static method
+          "ledger.css" "text/css"
+        ["ledger.jsx"] -> static method
+          "ledger.jsx" "text/jsx"
 
-        ["apple-touch-icon-precomposed-57.png"] -> static method "static/apple-touch-icon-precomposed-57.png" "image/png"
-        ["apple-touch-icon-precomposed-57@2x.png"] -> static method "static/apple-touch-icon-precomposed-57@2x.png" "image/png"
-        ["apple-touch-icon-precomposed-60@3x.png"] -> static method "static/apple-touch-icon-precomposed-60@3x.png" "image/png"
-        ["apple-touch-icon-precomposed-72.png"] -> static method "static/apple-touch-icon-precomposed-72.png" "image/png"
-        ["apple-touch-icon-precomposed-72@2x.png"] -> static method "static/apple-touch-icon-precomposed-72@2x.png" "image/png"
-        ["apple-touch-icon-precomposed-76.png"] -> static method "static/apple-touch-icon-precomposed-76.png" "image/png"
-        ["apple-touch-icon-precomposed-76@2x.png"] -> static method "static/apple-touch-icon-precomposed-76@2x.png" "image/png"
-        ["apple-touch-startup-image-320x460.png"] -> static method "static/apple-touch-startup-image-320x460.png" "image/png"
-        ["apple-touch-startup-image-320x460@2x.png"] -> static method "static/apple-touch-startup-image-320x460@2x.png" "image/png"
-        ["apple-touch-startup-image-320x548@2x.png"] -> static method "static/apple-touch-startup-image-320x548@2x.png" "image/png"
-        ["apple-touch-startup-image-375x647@2x.png"] -> static method "static/apple-touch-startup-image-375x647@2x.png" "image/png"
-        ["apple-touch-startup-image-394x736@3x.png"] -> static method "static/apple-touch-startup-image-394x736@3x.png" "image/png"
-        ["apple-touch-startup-image-414x716@3x.png"] -> static method "static/apple-touch-startup-image-414x716@3x.png" "image/png"
-        ["apple-touch-startup-image-748x1024.png"] -> static method "static/apple-touch-startup-image-748x1024.png" "image/png"
-        ["apple-touch-startup-image-748x1024@2x.png"] -> static method "static/apple-touch-startup-image-748x1024@2x.png" "image/png"
-        ["apple-touch-startup-image-768x1004.png"] -> static method "static/apple-touch-startup-image-768x1004.png" "image/png"
-        ["apple-touch-startup-image-768x1004@2x.png"] -> static method "static/apple-touch-startup-image-768x1004@2x.png" "image/png"
-        ["favicon.ico"] -> static method "static/favicon.ico" "image/x-icon"
-
+        -- Back end
         ["api", "entries"] -> case method of
           "GET" -> Actions.getEntries
           "POST" -> Actions.postEntries
@@ -55,6 +39,54 @@ route request =
           "PUT" -> Actions.putEntry
           "DELETE" -> Actions.deleteEntry
           _ -> Actions.notAllowed
+
+        -- Static
+        ["favicon.ico"] -> static method
+          "static/favicon.ico" "image/x-icon"
+
+        ["apple-touch-icon-precomposed-57.png"] -> static method
+          "static/apple-touch-icon-precomposed-57.png" "image/png"
+        ["apple-touch-icon-precomposed-57@2x.png"] -> static method
+          "static/apple-touch-icon-precomposed-57@2x.png" "image/png"
+        ["apple-touch-icon-precomposed-60@3x.png"] -> static method
+          "static/apple-touch-icon-precomposed-60@3x.png" "image/png"
+        ["apple-touch-icon-precomposed-72.png"] -> static method
+          "static/apple-touch-icon-precomposed-72.png" "image/png"
+        ["apple-touch-icon-precomposed-72@2x.png"] -> static method
+          "static/apple-touch-icon-precomposed-72@2x.png" "image/png"
+        ["apple-touch-icon-precomposed-76.png"] -> static method
+          "static/apple-touch-icon-precomposed-76.png" "image/png"
+        ["apple-touch-icon-precomposed-76@2x.png"] -> static method
+          "static/apple-touch-icon-precomposed-76@2x.png" "image/png"
+
+        ["apple-touch-startup-image-320x460.png"] -> static method
+          "static/apple-touch-startup-image-320x460.png" "image/png"
+        ["apple-touch-startup-image-320x460@2x.png"] -> static method
+          "static/apple-touch-startup-image-320x460@2x.png" "image/png"
+        ["apple-touch-startup-image-320x548@2x.png"] -> static method
+          "static/apple-touch-startup-image-320x548@2x.png" "image/png"
+        ["apple-touch-startup-image-375x647@2x.png"] -> static method
+          "static/apple-touch-startup-image-375x647@2x.png" "image/png"
+        ["apple-touch-startup-image-394x736@3x.png"] -> static method
+          "static/apple-touch-startup-image-394x736@3x.png" "image/png"
+        ["apple-touch-startup-image-414x716@3x.png"] -> static method
+          "static/apple-touch-startup-image-414x716@3x.png" "image/png"
+        ["apple-touch-startup-image-748x1024.png"] -> static method
+          "static/apple-touch-startup-image-748x1024.png" "image/png"
+        ["apple-touch-startup-image-748x1024@2x.png"] -> static method
+          "static/apple-touch-startup-image-748x1024@2x.png" "image/png"
+        ["apple-touch-startup-image-768x1004.png"] -> static method
+          "static/apple-touch-startup-image-768x1004.png" "image/png"
+        ["apple-touch-startup-image-768x1004@2x.png"] -> static method
+          "static/apple-touch-startup-image-768x1004@2x.png" "image/png"
+
+        -- Vendor
+        ["jsx.js"] -> static method
+          "vendor/JSXTransformer-0.12.0.js" "application/javascript"
+        ["react.js"] -> static method
+          "vendor/react-0.12.0.js" "application/javascript"
+        ["superagent.js"] -> static method
+          "vendor/superagent-0.20.0.js" "application/javascript"
 
         _ -> Actions.notFound
 
