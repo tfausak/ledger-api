39502a960d12ed2903f8886f6e5fde858d392519
Taylor Fausak <taylor@fausak.me>
2014-11-19 19:48:55 -0600

    Mock up how the interface might look with Bootstrap

--- data/ledger.jsx
+++ data/ledger.jsx
@@ -1,75 +1,51 @@
-var EntryBox = React.createClass({
-  transform: function(object) {
-    return {
-      amount: object.amount,
-      date: new Date(object.created),
-      key: object.number,
-      name: object.name,
-      number: object.number
-    };
-  },
-  handleEntrySubmit: function(entry) {
-    superagent.post('/api/entries?key=' + this.state.key, entry, function(response) {
-      this.setState({entries: [this.transform(response.body)].concat(this.state.entries)})
-    }.bind(this));
-  },
-  handleEntryDelete: function (number) {
-    superagent.del('/api/entries/' + number + '?key=' + this.state.key, function () {
-      this.getEntries();
-    }.bind(this));
-  },
-  handleEntryUpdate: function(entry) {
-    superagent.put('/api/entries/' + entry.number + '?key=' + this.state.key, entry, function() {
-      this.getEntries();
-    }.bind(this));
-  },
-  getInitialState: function() {
-    return {entries: [], key: window.location.hash.substring(1)}
-  },
-  componentDidMount: function() {
-    this.getEntries();
-  },
-  getEntries: function() {
-    superagent.get('/api/entries?key=' + this.state.key, function(response) {
-      this.setState({entries: response.body.map(this.transform)});
-    }.bind(this));
-  },
+var Ledger = React.createClass({
   render: function() {
     return (
       <div>
-        <EntryBalance entries={this.state.entries} />
-        <EntryForm onEntrySubmit={this.handleEntrySubmit} />
-        <EntryList
-          entries={this.state.entries}
-          onEntryDelete={this.handleEntryDelete}
-          onEntryUpdate={this.handleEntryUpdate}
-        />
+        <Navigation />
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-4">
+              <NewEntry />
+            </div>
+
+            <div className="col-sm-8">
+              <Entries />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 });
 
-var EntryBalance = React.createClass({
-  getBalance: function() {
-    return this.props.entries
-      .map(function(e) { return e.amount; })
-      .reduce(function(x, y) { return x + y; }, 0);
-  },
-  getClass: function() {
-    var balance = this.getBalance();
-
-    if (balance > 0) { return 'positive'; }
-    if (balance < 0) { return 'negative'; }
-  },
+var Navigation = React.createClass({
   render: function() {
     return (
-      <div className="balance">
-        <div className="balance-label">
-          Balance
+      <nav className="navbar navbar-default navbar-static-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="/">
+              Ledger
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+});
+
+var NewEntry = React.createClass({
+  render: function() {
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          Create an entry
         </div>
 
-        <div className={['balance-value', this.getClass()].join(' ')}>
-          ${this.getBalance().toFixed(2)}
+        <div className="panel-body">
+          <EntryForm />
         </div>
       </div>
     );
@@ -77,197 +53,81 @@ var EntryBalance = React.createClass({
 });
 
 var EntryForm = React.createClass({
-  handleSubmit: function(event) {
-    var typeNode = this.refs.type.getDOMNode();
-    var type = typeNode.value;
-    var amountNode = this.refs.amount.getDOMNode();
-    var amount = amountNode.valueAsNumber;
-    var nameNode = this.refs.name.getDOMNode();
-    var name = nameNode.value;
-
-    event.preventDefault();
-
-    if (isNaN(amount) || !isFinite(amount)) {
-      amountNode.focus();
-      return;
-    }
-
-    if (!name) {
-      nameNode.focus();
-      return;
-    }
-
-    if (type == 'credit') {
-      amount *= -1;
-    }
-
-    typeNode.value = 'credit';
-    typeNode.blur();
-    amountNode.value = '';
-    amountNode.blur();
-    nameNode.value = '';
-    nameNode.blur();
-
-    this.props.onEntrySubmit({amount: amount, name: name, number: 0});
-  },
   render: function() {
     return (
-      <fieldset className="form">
-        <legend className="form-legend">Create an entry</legend>
+      <form>
+        <div className="radio">
+          <label>
+            <input type="radio" name="type" defaultChecked />
+            Credit
+          </label>
+        </div>
 
-        <form onSubmit={this.handleSubmit}>
-          <ol className="form-list">
-            <li className="form-element">
-              <label className="form-label" htmlFor="type">Type</label>
-              <select className="form-input" ref="type" id="type" defaultValue="credit">
-                <option value="credit">Credit</option>
-                <option value="debit">Debit</option>
-              </select>
-            </li>
+        <div className="radio">
+          <label>
+            <input type="radio" name="type" />
+            Debit
+          </label>
+        </div>
 
-            <li className="form-element">
-              <label className="form-label" htmlFor="amount">Amount</label>
-              <input className="form-input" type="number" ref="amount" placeholder="7.31" id="amount" min="0" step="any" />
-            </li>
+        <div className="form-group">
+          <label htmlFor="amount">
+            Amount
+          </label>
+          <div className="input-group">
+            <div className="input-group-addon">
+              $
+            </div>
+            <input type="number" className="form-control" id="amount" placeholder="7.31" />
+          </div>
+        </div>
 
-            <li className="form-element">
-              <label className="form-label" htmlFor="name">Name</label>
-              <input className="form-input" ref="name" placeholder="Lunch at Freebirds" id="name" />
-            </li>
+        <div className="form-group">
+          <label htmlFor="name">
+            Name
+          </label>
+          <input type="text" className="form-control" id="name" placeholder="Lunch at Freebirds" />
+        </div>
 
-            <li className="form-element">
-              <input className="form-submit" type="submit" value="Create" />
-            </li>
-          </ol>
-        </form>
-      </fieldset>
+        <button type="submit" className="btn btn-primary">
+          Create
+        </button>
+      </form>
     );
   }
 });
 
-var EntryList = React.createClass({
-  handleEntryDelete: function(number) {
-    this.props.onEntryDelete(number);
-  },
-  handleEntryUpdate: function(entry) {
-    this.props.onEntryUpdate(entry);
-  },
-  renderEntryElements: function() {
-    return this.props.entries.sort(function (a, b) {
-      return b.date - a.date;
-    }).map(function (entry) {
-      return (
-        <EntryElement
-          amount={entry.amount}
-          date={entry.date}
-          key={entry.key}
-          name={entry.name}
-          number={entry.number}
-          onEntryDelete={this.handleEntryDelete}
-          onEntryUpdate={this.handleEntryUpdate}
-        />
-      );
-    }.bind(this));
-  },
+var Entries = React.createClass({
   render: function() {
     return (
-      <ol className="entries">
-        {this.renderEntryElements()}
-      </ol>
+      <div className="list-group">
+        <Entry />
+      </div>
     );
   }
 });
 
-var EntryElement = React.createClass({
-  getInitialState: function() {
-    return {editing: false};
-  },
-  handleEditClick: function(e) {
-    this.setState({editing: true});
-    e.preventDefault();
-  },
-  handleDeleteClick: function(e) {
-    if (window.confirm('Are you sure you want to delete this entry?')) {
-      this.props.onEntryDelete(this.props.number);
-    }
-    e.preventDefault();
-  },
-  handleSaveClick: function(e) {
-    var amount = this.refs.amount.getDOMNode().valueAsNumber;
-    var name = this.refs.name.getDOMNode().value;
-
-    if (!isNaN(amount) && isFinite(amount) && name) {
-      var entry = {amount: amount, name: name, number: this.props.number};
-      this.props.onEntryUpdate(entry);
-      this.setState({editing: false});
-    }
-
-    e.preventDefault();
-  },
-  handleCancelClick: function(e) {
-    this.setState({editing: false});
-    e.preventDefault();
-  },
-  getClass: function() {
-    if (this.props.amount > 0) { return 'positive'; }
-    if (this.props.amount < 0) { return 'negative'; }
-  },
-  renderShow: function() {
-    return (
-      <li className="entry">
-        <div className={['entry-amount', this.getClass()].join(' ')}>
-          ${this.props.amount.toFixed(2)}
-        </div>
-
-        <div className="entry-name">
-          {this.props.name}
-        </div>
-
-        <div className="entry-date">
-          <time dateTime={this.props.date.toISOString()}>
-            {this.props.date.toLocaleDateString()}
-          </time>
-        </div>
-
-        <button className="entry-edit" onClick={this.handleEditClick}>Edit</button>
-        <button className="entry-delete" onClick={this.handleDeleteClick}>Delete</button>
-      </li>
-    );
-  },
-  renderEdit: function() {
-    return (
-      <li className="entry">
-        <form onSubmit={this.handleSaveClick}>
-          <div className="entry-amount">
-            <input type="number" ref="amount" defaultValue={this.props.amount} step="any" />
-          </div>
-
-          <div className="entry-name">
-            <input ref="name" defaultValue={this.props.name} />
-          </div>
-
-          <div className="entry-date">
-            <time dateTime={this.props.date.toISOString()}>
-              {this.props.date.toLocaleDateString()}
-            </time>
-          </div>
-
-          <input type="submit" value="Save" className="entry-save" />
-          <button className="entry-cancel" onClick={this.handleCancelClick}>Cancel</button>
-        </form>
-      </li>
-    );
-  },
+var Entry = React.createClass({
   render: function() {
-    if (this.state.editing) {
-      return this.renderEdit();
-    } else {
-      return this.renderShow();
-    }
+    return (
+      <div className="list-group-item">
+        <div className="lead">
+          <div className="pull-right">
+            <div className="label label-danger">
+              $-7.31
+            </div>
+          </div>
+
+          Lunch at Freebirds
+        </div>
+
+        November 19, 2014
+      </div>
+    );
   }
 });
 
 React.render(
-  <EntryBox />,
-  document.getElementById('content')
+  <Ledger />,
+  document.getElementById('react')
 );
