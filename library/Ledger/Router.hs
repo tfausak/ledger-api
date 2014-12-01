e7eecc8e17c7cbe4f56110f6d93d9ae566b82848
Taylor Fausak <taylor@fausak.me>
2014-11-30 18:19:32 -0600

    Remove front end

--- library/Ledger/Router.hs
+++ library/Ledger/Router.hs
@@ -2,84 +2,25 @@
 
 module Ledger.Router where
 
-import           Ledger.Actions         (Action)
-import qualified Ledger.Actions         as Actions
-import           Ledger.Utilities       (file)
+import           Ledger.Actions (Action)
+import qualified Ledger.Actions as Actions
 
-import           Control.Monad.IO.Class (liftIO)
-import           Data.ByteString        (ByteString)
-import           Network.HTTP.Types     (Method)
-import           Network.Wai            (Request, pathInfo, requestMethod)
+import           Network.Wai    (Request, pathInfo, requestMethod)
 
 route :: Request -> Action
 route request =
   let path = pathInfo request
       method = requestMethod request
   in  case path of
-        -- Front end
-        [] -> static method
-          "ledger.html" "text/html"
-        ["ledger.manifest"] -> static method
-          "ledger.manifest" "text/cache-manifest"
-        ["all.min.css"] -> static method
-          "all.min.css" "text/css"
-        ["all.min.js"] -> static method
-          "all.min.js" "application/javascript"
-
-        -- Back end
         ["api", "entries"] -> case method of
           "GET" -> Actions.getEntries
           "POST" -> Actions.postEntries
           _ -> Actions.notAllowed
+
         ["api", "entries", entryId] -> case method of
           "GET" -> Actions.getEntry entryId
           "PUT" -> Actions.putEntry entryId
           "DELETE" -> Actions.deleteEntry entryId
           _ -> Actions.notAllowed
 
-        -- Static
-        ["favicon.ico"] -> static method
-          "static/favicon.ico" "image/x-icon"
-
-        ["apple-touch-icon-precomposed-57.png"] -> static method
-          "static/apple-touch-icon-precomposed-57.png" "image/png"
-        ["apple-touch-icon-precomposed-57@2x.png"] -> static method
-          "static/apple-touch-icon-precomposed-57@2x.png" "image/png"
-        ["apple-touch-icon-precomposed-60@3x.png"] -> static method
-          "static/apple-touch-icon-precomposed-60@3x.png" "image/png"
-        ["apple-touch-icon-precomposed-72.png"] -> static method
-          "static/apple-touch-icon-precomposed-72.png" "image/png"
-        ["apple-touch-icon-precomposed-72@2x.png"] -> static method
-          "static/apple-touch-icon-precomposed-72@2x.png" "image/png"
-        ["apple-touch-icon-precomposed-76.png"] -> static method
-          "static/apple-touch-icon-precomposed-76.png" "image/png"
-        ["apple-touch-icon-precomposed-76@2x.png"] -> static method
-          "static/apple-touch-icon-precomposed-76@2x.png" "image/png"
-
-        ["apple-touch-startup-image-320x460.png"] -> static method
-          "static/apple-touch-startup-image-320x460.png" "image/png"
-        ["apple-touch-startup-image-320x460@2x.png"] -> static method
-          "static/apple-touch-startup-image-320x460@2x.png" "image/png"
-        ["apple-touch-startup-image-320x548@2x.png"] -> static method
-          "static/apple-touch-startup-image-320x548@2x.png" "image/png"
-        ["apple-touch-startup-image-375x647@2x.png"] -> static method
-          "static/apple-touch-startup-image-375x647@2x.png" "image/png"
-        ["apple-touch-startup-image-394x736@3x.png"] -> static method
-          "static/apple-touch-startup-image-394x736@3x.png" "image/png"
-        ["apple-touch-startup-image-414x716@3x.png"] -> static method
-          "static/apple-touch-startup-image-414x716@3x.png" "image/png"
-        ["apple-touch-startup-image-748x1024.png"] -> static method
-          "static/apple-touch-startup-image-748x1024.png" "image/png"
-        ["apple-touch-startup-image-748x1024@2x.png"] -> static method
-          "static/apple-touch-startup-image-748x1024@2x.png" "image/png"
-        ["apple-touch-startup-image-768x1004.png"] -> static method
-          "static/apple-touch-startup-image-768x1004.png" "image/png"
-        ["apple-touch-startup-image-768x1004@2x.png"] -> static method
-          "static/apple-touch-startup-image-768x1004@2x.png" "image/png"
-
         _ -> Actions.notFound
-
-static :: Method -> FilePath -> ByteString -> Action
-static method path contentType = case method of
-  "GET" -> liftIO (file path contentType)
-  _ -> Actions.notAllowed
--- library/Ledger/Utilities.hs
+++ library/Ledger/Utilities.hs
@@ -2,30 +2,14 @@
 
 module Ledger.Utilities where
 
-import           Paths_ledger         (getDataFileName)
+import           Paths_ledger       (getDataFileName)
 
-import           Data.Aeson           (ToJSON, encode)
-import           Data.ByteString      (ByteString)
-import           Data.ByteString.Lazy (readFile)
-import           Data.Map             (fromList, insert, toList)
-import           Network.HTTP.Types   (ResponseHeaders, Status, hCacheControl,
-                                       hContentType, status200)
-import           Network.Wai          (Response, responseLBS)
-import           Prelude              hiding (readFile)
-import           System.Environment   (lookupEnv)
-import           System.FilePath      ((</>))
-
-file :: FilePath -> ByteString -> IO Response
-file path contentType = do
-  name <- getFileName path
-  body <- readFile name
-  let status = status200
-  let headers =
-        [ (hCacheControl, "max-age=86400")
-        , (hContentType, contentType)
-        ]
-  let response = responseLBS status headers body
-  return response
+import           Data.Aeson         (ToJSON, encode)
+import           Data.Map           (fromList, insert, toList)
+import           Network.HTTP.Types (ResponseHeaders, Status, hContentType)
+import           Network.Wai        (Response, responseLBS)
+import           System.Environment (lookupEnv)
+import           System.FilePath    ((</>))
 
 getFileName :: FilePath -> IO FilePath
 getFileName path = do
