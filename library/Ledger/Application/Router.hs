c8967872d1121b0a4c68e63157e132ee7b1914df
Taylor Fausak <taylor@fausak.me>
2014-12-09 08:25:25 -0600

    Separate routing from requests

--- library/Ledger/Application/Router.hs
+++ library/Ledger/Application/Router.hs
@@ -4,33 +4,41 @@ module Ledger.Application.Router where
 
 import Ledger.Application.Action
 
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import Network.HTTP.Types (Method)
 import Network.Wai (Request, pathInfo, requestMethod)
 
 route :: Request -> Action
 route request =
     let path = pathInfo request
         method = requestMethod request
-    in  case path of
-            [] -> case method of
-                "GET" -> getRootA
-                _ -> notAllowedA
+    in  maybe notFoundA (fromMaybe notAllowedA) (route' path method)
 
-            ["entries"] -> case method of
-                "GET" -> getEntriesA
-                "POST" -> postEntriesA
-                _ -> notAllowedA
-            ["entries", eid] -> case method of
-                "GET" -> getEntryA eid
-                "PUT" -> putEntryA eid
-                "DELETE" -> deleteEntryA eid
-                _ -> notAllowedA
+route' :: [Text] -> Method -> Maybe (Maybe Action)
+route' path method = case path of
+    [] -> Just $ case method of
+        "GET" -> Just getRootA
+        _ -> Nothing
 
-            ["keys"] -> case method of
-                "POST" -> postKeysA
-                _ -> notAllowedA
-            ["keys", kid] -> case method of
-                "GET" -> getKeyA kid
-                "DELETE" -> deleteKeyA kid
-                _ -> notAllowedA
+    ["entries"] -> Just $ case method of
+        "GET" -> Just getEntriesA
+        "POST" -> Just postEntriesA
+        _ -> Nothing
 
-            _ -> notFoundA
+    ["entries", eid] -> Just $ case method of
+        "GET" -> Just (getEntryA eid)
+        "PUT" -> Just (putEntryA eid)
+        "DELETE" -> Just (deleteEntryA eid)
+        _ -> Nothing
+
+    ["keys"] -> Just $ case method of
+        "POST" -> Just postKeysA
+        _ -> Nothing
+
+    ["keys", kid] -> Just $ case method of
+        "GET" -> Just (getKeyA kid)
+        "DELETE" -> Just (deleteKeyA kid)
+        _ -> Nothing
+
+    _ -> Nothing
