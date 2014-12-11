fc89575c766b7a41de4fb6594bc686a5bda5829d
Taylor Fausak <taylor@fausak.me>
2014-12-10 21:26:06 -0600

    Configure HLint

--- test-suite/HLint.hs
+++ test-suite/HLint.hs
@@ -6,6 +6,13 @@ import System.Exit (exitFailure)
 
 main :: IO ()
 main = do
-    let arguments = ["--color", "executable", "library", "test-suite"]
+    let arguments =
+            [ "lint"
+            , "--color"
+            , "executable"
+            , "library"
+            , "test-suite"
+            , "Setup.hs"
+            ]
     suggestions <- hlint arguments
     unless (null suggestions) exitFailure
